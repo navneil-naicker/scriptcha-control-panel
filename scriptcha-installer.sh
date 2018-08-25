a50c5f9db5643e55877e01d5a66120a93dba1e9f
Navneil Naicker <navneil.naicker@gmail.com>
2018-08-25 21:32:38 +1200

    Update scriptcha-installer.sh

--- scriptcha-installer.sh
+++ scriptcha-installer.sh
@@ -5,6 +5,7 @@ yum -y update
 yum -y install httpd
 
 curl -O http://www.scriptcha.com/scriptcha.zip
+unzip scriptcha.zip
 
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
